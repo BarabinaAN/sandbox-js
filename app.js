91e9c8a33bad96462c30653636138f54cd7d6ebe
Natalya Barabina <barabash4ka@gmail.com>
2020-09-02 15:35:04 +0300

    work with numbers

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+let pi = +Math.PI.toFixed(2)
+
+let maxValue = Math.max(15, 11, 16, 12, 51, 12, 13, 51)
+let rndA = +(Math.random()*10).toFixed(2);
+let num = 5;
+let rndB = Math.floor(Math.random()*num+1);
+let sum = (0.6*10+0.7*10)/10
+let strToNum = parseInt('100$');
+console.log(strToNum, typeof strToNum);
