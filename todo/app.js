018472cf5e663551297dae3fdaf2f2a3353045a9
Natalia B <barabash4ka@gmail.com>
2020-09-29 16:28:45 +0300

    add select theme to the project

--- todo/app.js
+++ todo/app.js
@@ -32,7 +32,75 @@ const tasks = [
 ];
 
 (function (arrOfTasks) {
-  
+  // theme variables
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+  let currentTheme = 'default'
+
   // transformation [] => {}
   const objOfTasks = arrOfTasks.reduce((acc, task) => {
     acc[task._id] = task
@@ -44,10 +112,32 @@ const tasks = [
   const form = document.forms['addTask']
   const inputTitle = form.elements['title']
   const inputBody = form.elements['body']
+  const themeSelect = document.getElementById('themeSelect');
 
   // events
   form.addEventListener('submit', onFormSubmitHendler)
   ul.addEventListener('click', onDeleteHendler)
+  themeSelect.addEventListener('change', onThemeSelect)
+
+  // select theme
+  function onThemeSelect(e) {
+    const selectVal = themeSelect.value
+    const isSelected = confirm(`Change theme to the: ${selectVal}`)
+    if (!isSelected) {
+      themeSelect.value = currentTheme
+      return
+    }
+    setTheme(selectVal)
+    currentTheme = selectVal
+  }
+
+  // set current theme
+  function setTheme(name) {
+    const theme = themes[name]
+    Object.entries(theme).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value)
+    })
+  }
 
   // submit form && add new list item to the DOM
   function onFormSubmitHendler(e) {
