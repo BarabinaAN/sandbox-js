efa3bafa296968adae92a4afc821ed47ee8f3bb5
Natalia B <barabash4ka@gmail.com>
2020-10-02 13:17:58 +0300

    add new user && output on the page

--- ajax-2/app.js
+++ ajax-2/app.js
@@ -1,3 +1,4 @@
+// get users
 function getData(fun) {
   const _url = 'https://jsonplaceholder.typicode.com/users'
   const request = new XMLHttpRequest();
@@ -11,31 +12,48 @@ function getData(fun) {
   request.send()
 }
 
-getData(res => {
-  renderUsers(res)
-})
+// send new user
+function sendData(body, fun) {
+  const _url = 'https://jsonplaceholder.typicode.com/users'
+  const request = new XMLHttpRequest();
 
-// 1. get data
-// 2. render data to page
-// 3. create function add new user on the page
+  request.open('post', _url)
+  request.addEventListener('load', () => {
+    if (request.readyState !== 4) return console.error('Error, can not get data')
+    const res = JSON.parse(request.responseText)
+    fun(res)
+  })
+  request.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+  request.send(JSON.stringify(body))
+}
 
-const userList = document.querySelector('.user-list')
+// init function
+getData(res => renderUsers(res))
 
+
+// DOM nodes
+const ul = document.querySelector('.user-list')
+const form = document.forms[0]
+const [...items] = form.querySelectorAll('input')
+
+// variables
+let userslenght = 0
+
+
+// render all users
 function renderUsers(users) {
   let li = users.reduce((acc, user) => acc + liTemplate(user), '')
-  const ul = ulTemplate(li)
-  userList.appendChild(ul)
-}
-
-function ulTemplate(li) {
-  const fragment = document.createDocumentFragment()
-  const ul = document.createElement('ul')
-  ul.classList.add('list-group')
   ul.insertAdjacentHTML('afterbegin', li)
-  fragment.appendChild(ul)
-  return fragment
+  userslenght = users.length
 }
 
+// render one user
+function renderUser(user) {
+  let li = liTemplate(user)
+  ul.insertAdjacentHTML('afterbegin', li)
+}
+
+// html temlate 'li' node for one user
 function liTemplate({ name, email, website, phone }) {
   return `<li class="list-group-item mb-2">
       <p class="text-success text-uppercase"> ${name} </p>
@@ -44,3 +62,33 @@ function liTemplate({ name, email, website, phone }) {
       <p> <span class="text-info">phone:</span> ${phone} </p>
     </li>`
 }
+
+// create new user && send {} => service
+function createNewUser(arr) {
+  let user = { id: ++userslenght }
+  let isError = false
+
+  arr.map(({ name, value }) => {
+    isError = checkError(name, value)
+    user[name] = value
+  })
+
+  if (isError) return isError;
+  return sendData(user, (res) => renderUser(res))
+}
+
+// check field on error
+function checkError(name, value) {
+  if (!value) {
+    const message = `field: "${name}" is empty!!!`
+    console.error(message)
+    return true
+  }
+  return false
+}
+
+// event 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  createNewUser(items)
+})
