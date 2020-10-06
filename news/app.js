c895255bdd3e4e840da2f1d11cb5725970012a88
Natalia B <barabash4ka@gmail.com>
2020-10-06 22:37:40 +0300

    load && output all news

--- news/app.js
+++ news/app.js
@@ -56,7 +56,70 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+const service = (function () {
+  const apiKey = '8f13bbc9f4cb4be882690b31743b35ed'
+  const apiUrl = 'http://news-api-v2.herokuapp.com'
+  return {
+    topheadlines(country = 'ua', cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        cb
+      )
+    },
+    everising(query, cb) {
+      http.get(
+        `${apiUrl}/everything?q=${query}&apiKey=${apiKey}`,
+        cb
+      )
+    }
+  }
+})()
+
 //  init selects
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   M.AutoInit();
+  loadNews();
 });
+
+// load
+function loadNews() {
+  service.topheadlines('ua', getResponse)
+}
+
+function getResponse(err, res) {
+  renderNews(res.articles)
+}
+
+//
+function renderNews(news) {
+  const container = document.querySelector('.news-container .row')
+  let fragment = ''
+
+  news.forEach(item => {
+    const newsItem = newsItemTemplate(item)
+    fragment += newsItem
+  })
+  
+  container.insertAdjacentHTML('afterbegin', fragment)
+}
+
+function newsItemTemplate({ urlToImage, title, url, description }) {
+  return `
+    <div class="col s12">
+      <div class="card">
+        <div class="card-image">
+          <img src="${urlToImage}" />
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+
